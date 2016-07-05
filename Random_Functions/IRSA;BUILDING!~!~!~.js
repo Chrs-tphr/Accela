e8ca4d6f68b2430ae1817404964c1ab9a7962ed0
Christopher <cisensano@yahoo.com>
2016-07-04 21:52:47 -0700

    Copied from Accela training

---
+++ Random_Functions/IRSA;BUILDING!~!~!~.js
@@ -0,0 +1,5 @@
+//post training exercise 3 START
+if(inspType == "Building Final" && inspResult == "Passed" && isTaskActive("Inspection")){
+	closeTask("Inspection","Final Inspection Complete","Closed automatically by Script","Closed automatically by Script");
+}
+//post training exercise 3 END
