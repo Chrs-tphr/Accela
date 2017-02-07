3626a512d8e35b1e15c6f43739eb0b9e95badb1e
Christopher <cisensano@yahoo.com>
2017-02-07 07:58:41 -0800

    updates to structure

--- Random_Functions/getAssignedStaff.js
+++
@@ -1,8 +0,0 @@
-function getAssignedStaff(){
-	var cdScriptObjResult = aa.cap.getCapDetail(capId);
-	var cdScriptObj = cdScriptObjResult.getOutput();
-	var cd = cdScriptObj.getCapDetailModel();
-	var currentAssignedStaff = cd.getAsgnStaff();
-	logDebug("CAP Assigned to: "+currentAssignedStaff);
-	return currentAssignedStaff;
-}
