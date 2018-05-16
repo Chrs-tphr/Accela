cc993d638c1ab46d1f60bf7a11b11c8ceec4f376
Christopher <cisensano@yahoo.com>
2018-05-15 21:56:19 -0700

    Updated to log string dates

--- test/emseScriptTester-GetAllWorkflowDataFromRecord.js
+++ test/emseScriptTester-GetAllWorkflowDataFromRecord.js
@@ -1,11 +1,11 @@
-var myCapId = "replaceWithAltId";
+var myCapId = "18BC00094";
 var myUserId = "ADMIN";
 
 /* ASB  */  //var eventName = "ApplicationSubmitBefore";
 /* ASA  */  //var eventName = "ApplicationSubmitAfter";
 /* ASUB  */  //var eventName = "ApplicationStatusUpdateBefore";
 /* ASUA  */  //var eventName = "ApplicationStatusUpdateAfter";
-/* WTUA */  var eventName = "WorkflowTaskUpdateAfter"; wfTask = "taskName"; wfStatus = "taskStatus"; wfDateMMDDYYYY = "01/01/2016";
+/* WTUA */  //var eventName = "WorkflowTaskUpdateAfter"; wfTask = "taskName"; wfStatus = "taskStatus"; wfDateMMDDYYYY = "01/01/2016";
 /* WTUB */  //var eventName = "WorkflowTaskUpdateBefore"; wfTask = "taskName"; wfStatus = "taskStatus";  wfDateMMDDYYYY = "01/01/2016";
 /* IRSA */  //var eventName = "InspectionResultSubmitAfter"; inspResult = "result"; inspResultComment = "comment";  inspType = "inspName"; wfTask = "taskName";
 /* ISA  */  //var eventName = "InspectionScheduleAfter"; inspType = "inspName";
@@ -14,10 +14,10 @@ var myUserId = "ADMIN";
 /* ASIUA */ //var eventName = "ApplicationSpecificInfoUpdateAfter";
 /* WTUB */  //var eventName = "WorkflowAdhocTaskUpdateBefore";
 /* WTUA */  //var eventName = "WorkflowAdhocTaskUpdateAfter";
-/* DUA */  //var eventName = "DocumentUploadAfter";
+/* DUA */  var eventName = "DocumentUploadAfter";
 
 var useProductScript = true;  // set to true to use the "productized" master scripts (events->master scripts), false to use scripts from (events->scripts)
-var runEvent = false; // set to true to simulate the event and run all std choices/scripts for the record type.  
+var runEvent = true; // set to true to simulate the event and run all std choices/scripts for the record type.  
 
 /* master script code don't touch */ aa.env.setValue("EventName",eventName); var vEventName = eventName;  var controlString = eventName;  var tmpID = aa.cap.getCapID(myCapId).getOutput(); if(tmpID != null){aa.env.setValue("PermitId1",tmpID.getID1()); 	aa.env.setValue("PermitId2",tmpID.getID2()); 	aa.env.setValue("PermitId3",tmpID.getID3());} aa.env.setValue("CurrentUserID",myUserId); var preExecute = "PreExecuteForAfterEvents";var documentOnly = false;var SCRIPT_VERSION = 3.0;var useSA = false;var SA = null;var SAScript = null;var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_FOR_EMSE"); if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") { 	useSA = true; 		SA = bzr.getOutput().getDescription();	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_INCLUDE_SCRIPT"); 	if (bzr.getSuccess()) { SAScript = bzr.getOutput().getDescription(); }	}if (SA) {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",SA,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",SA,useProductScript));	/* force for script test*/ showDebug = true; eval(getScriptText(SAScript,SA,useProductScript));	}else {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useProductScript));	}	eval(getScriptText("INCLUDES_CUSTOM",null,useProductScript));if (documentOnly) {	doStandardChoiceActions2(controlString,false,0);	aa.env.setValue("ScriptReturnCode", "0");	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");	aa.abortScript();	}var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX",vEventName);var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";var doStdChoices = true;  var doScripts = false;var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice ).getOutput().size() > 0;if (bzr) {	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"STD_CHOICE");	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"SCRIPT");	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	}	function getScriptText(vScriptName, servProvCode, useProductScripts) {	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();	vScriptName = vScriptName.toUpperCase();	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();	try {		if (useProductScripts) {			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);		} else {			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");		}		return emseScript.getScriptText() + "";	} catch (err) {		return "";	}}logGlobals(AInfo); if (runEvent && typeof(doStandardChoiceActions) == "function" && doStdChoices) try {doStandardChoiceActions(controlString,true,0); } catch (err) { logDebug(err.message) } if (runEvent && typeof(doScriptActions) == "function" && doScripts) doScriptActions(); var z = debug.replace(/<BR>/g,"\r");  aa.print(z); 
 
@@ -28,6 +28,22 @@ var runEvent = false; // set to true to simulate the event and run all std choic
 try {
 	showDebug = true;
 //INSERT TEST CODE START
+	
+	function viewObj(obj){
+		var outputArray = [];
+		for(var key in obj){
+			if(typeof obj[key] == 'function')
+				outputArray.push(key + '()');
+			else
+				outputArray.push(key + ": " + obj[key]);
+		}
+		outputArray.sort();
+		for(i=1;i<outputArray.length;i++){
+			logDebug(outputArray[i]);
+		}
+	}
+	
+//	addAdHocTask("ADHOC_WORKFLOW", "Document Intake", "Test", "CHRIS.GODWIN");
 	
 	//get workflow tasks from record
 	var currentWfTaskStepNumber = aa.env.getValue("SD_STP_NUM");
@@ -42,6 +58,9 @@ try {
 			var thisActiveFlag = thisTask.getActiveFlag(); logDebug("   ActiveFlag: "+thisActiveFlag);
 			var thisAssignedStaff = thisTask.getAssignedStaff(); logDebug("   AssignedStaff: "+thisAssignedStaff);
 			var thisAssignmentDate = thisTask.getAssignmentDate(); logDebug("   AssignmentDate: "+thisAssignmentDate);
+			if(thisAssignmentDate){
+				logDebug("   AssignmentDate: "+thisAssignmentDate.month+"/"+thisAssignmentDate.dayOfMonth+"/"+thisAssignmentDate.year);
+			}
 			var thisBillable = thisTask.getBillable(); logDebug("   Billable: "+thisBillable);
 			var thisCapID = thisTask.getCapID(); logDebug("   CapID: "+thisCapID);
 			var thisCompleteFlag = thisTask.getCompleteFlag(); logDebug("   CompleteFlag: "+thisCompleteFlag);
@@ -53,6 +72,9 @@ try {
 			var thisDispositionDateString = thisTask.getDispositionDateString(); logDebug("   DispositionDateString: "+thisDispositionDateString);
 			var thisDispositionNote = thisTask.getDispositionNote(); logDebug("   DispositionNote: "+thisDispositionNote);
 			var thisDueDate = thisTask.getDueDate(); logDebug("   DueDate: "+thisDueDate);
+			if(thisDueDate){
+				logDebug("   DueDate: "+thisDueDate.month+"/"+thisDueDate.dayOfMonth+"/"+thisDueDate.year);
+			}
 			var thisEndTime = thisTask.getEndTime(); logDebug("   EndTime: "+thisEndTime);
 			var thisHoursSpent = thisTask.getHoursSpent(); logDebug("   HoursSpent: "+thisHoursSpent);
 			var thisNextTaskID = thisTask.getNextTaskID(); logDebug("   NextTaskID: "+thisNextTaskID);
